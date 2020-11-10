26cd3a4f5da3710c048ec9cd138ff1e9e17aec15
matiascg <matiascargon@hotmail.com>
2020-11-10 21:08:40 +0100

    Created cpu registers

--- src/CPU6502.h
+++ src/CPU6502.h
@@ -4,9 +4,33 @@
 
 #ifndef NESEMULATOR_CPU6502_H
 #define NESEMULATOR_CPU6502_H
-
+#include <cstdint>
 
 class CPU6502 {
+    int8_t X;
+    int8_t Y;
+    int8_t ACC;
+    //Programm counter
+    int16_t PC;
+    //Stack Pointer
+    int8_t SP;
+    //Flags
+    // Zero
+    bool Z;
+    // Carry
+    bool C;
+    // Interrupt disable
+    bool I;
+    // Decimal (not used)
+    bool D;
+    // Overflow
+    bool V;
+    // Negative
+    bool N;
+    // B Flag
+    bool B;
+
+
 
 };
 
