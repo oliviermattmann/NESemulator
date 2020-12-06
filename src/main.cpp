19af3f0eb2ca045f86ca721cc3f775cb1fdd73e9
olivierM <olivier.mattmann@stud.unibas.ch>
2020-12-06 15:37:26 +0100

    simplified main.cpp a bit

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,12 @@
 #include "Bus.h"
-#include "CPU6502.h"
-#include "Cartridge.h"
-#include "tests.h"
-#include <cstdio>
 #include "CompactLogger.h"
 
 int main(){
 
     Bus *bus = new Bus();
-    CPU6502 *cpu = new CPU6502(bus);
-    Cartridge *cartridge = new Cartridge("../roms/DonkeyKong.nes");
+    Cartridge *cartridge = new Cartridge("../roms/DonkeyKong");
     bus->insertCartridge(*cartridge);
-    std::cout << std::endl << hex << (int)cpu->read(0x6000);
-    cpu->run();
+    bus->start();
 
 
 
