43143ddea973448ca0bae3e4aadc96a1e9b67e0d
olivierM <olivier.mattmann@stud.unibas.ch>
2021-01-06 15:12:03 +0100

    commented Bus.cpp and Bus.h

--- src/Bus.cpp
+++ src/Bus.cpp
@@ -6,47 +6,60 @@
 
 Bus::Bus() {
     cartridge = Cartridge();
-    masterClock = 0;
+    controller = 0x00;
+    controller_status = 0x00;
+
     for(uint8_t & text : RAM) {
         text = 0x00;
     }
-    controller = 0x00;
-    controller_status = 0x00;
 }
 Bus::~Bus() {
     //does nothing
 }
 
 uint8_t Bus::busRead(uint16_t address){
+    //reading from main memory
     if(address <= 0x1FFF) {
         return RAM[address & 0x07FF];
-    } else if (address >= 0x2000 & address <= 0x3FFF) {
-        return  ppu->readCPU(address & 0x0007);
     }
+    //read from ppu registers
+    else if (address >= 0x2000 & address <= 0x3FFF) {
+        return  ppu->readCPU(address);
+    }
+    //Address range for the cartridge data
     else if (address >= 0x8000 & address <= 0xFFFF) {
         return cartridge.prgData[address%cartridge.BANKSIZE];
-        //for nestest.nes this does not work us address%cartridge.BANKSIZE as index instead
     }
+    //Controller
     else if (address >= 0x4016 && address <= 0x4017)
     {
-        // Read out the MSB of the controller status word
-        uint8_t temp = (controller_status & 0x80) > 0;
+        //Read out the MSB of the controller status word
+        //read 8 times in a row to get all the data
+        uint8_t bit = (controller_status & 0x80) > 0;
         controller_status <<= 1;
-        return temp;
+        return bit;
     }
     return -1;
 }
 void Bus::busWrite(uint16_t address, uint8_t data){
+    //writes to the main memory
     if (address <= 0x1FFF) {
+        //address is mirrored because the memory is is only 2KB
         RAM[address & 0x07FF] = data;
-    } else if (address >= 0x4016 && address <= 0x4017) {
+    }
+    //write to this address transfers the controller byte buffered into the status for the cpu to be read 1 bit at a time
+    else if (address >= 0x4016 && address <= 0x4017) {
+        //currently only one controller
         controller_status = controller;
-    } else if (address >= 0x2000 & address <= 0x3FFF) {
-        ppu->writeCPU(address, data);
-    } else if (address == 0x4014) {
+    }
+    //A write to this address range goes to the ppu, the address further processed in the ppu
+    else if (address >= 0x2000 & address <= 0x3FFF) {
         ppu->writeCPU(address, data);
     }
-
+    //a write to this address signals an incoming dma transfer
+    else if (address == 0x4014) {
+        ppu->writeCPU(address, data);
+    }
 }
 
 void Bus::insertCartridge(Cartridge cartridge) {
--- src/Bus.h
+++ src/Bus.h
@@ -11,23 +11,26 @@
 #include "PPU2C02.h"
 #include "CPU6502.h"
 
-
+//forward declarations
 class Cartridge;
 class PPU2C02;
 class CPU6502;
-
 
 class Bus {
 
 public:
     uint8_t RAM[2048];
-    uint8_t masterClock;
     Cartridge cartridge;
+    //controller where NESEmulator writes to
     uint8_t controller;
+    //controller the cpu reads from
     uint8_t controller_status;
+
     PPU2C02 *ppu = nullptr;
     CPU6502 *cpu = nullptr;
 
+
+    //Both the ppu and nesemulator have access to this class, so the dma variables are the here
     bool dma = false;
     bool idle = true;
 
@@ -40,18 +43,12 @@ public:
 
     ~Bus();
 
+    //Reads
     uint8_t busRead(uint16_t address);
-
+    //writes
     void busWrite(uint16_t address, uint8_t data);
-
+    //initialized the cartridge
     void insertCartridge(Cartridge cartridge);
 
-    void busClock();
-
-    //void start();
-
-
 };
-
-
 #endif //NESEMULATOR_BUS_H
